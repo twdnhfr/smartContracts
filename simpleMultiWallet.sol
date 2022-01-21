8e640656ac52607bcb327950172914433f0a3a2a
Tobias <ibot.root@gmail.com>
2022-01-22 00:28:46 +0100

    Create simpleMultiWallet.sol

---
+++ simpleMultiWallet.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
+
+contract SimpleWallet {
+    
+    address public owner;
+
+    mapping(address => uint256) funds; 
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {
+        depositFunds();
+    }
+
+    function destroy() public payable {
+        require(msg.sender == owner);
+
+        selfdestruct(payable(owner));
+    }
+
+    function withdrawFunds(uint256 _amount) public payable {
+        require(_amount <= address(this).balance, "Not enough in wallet");
+        require(_amount <= funds[msg.sender], "Not enough allocated for you");
+
+        funds[msg.sender] -= _amount;
+
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function depositFunds() public payable {
+        funds[msg.sender] += msg.value;
+        funds[owner] = address(this).balance;
+    }
+
+    function getTotalBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getUserBalance() public view returns (uint256) {
+        return funds[msg.sender];
+    }
+}
